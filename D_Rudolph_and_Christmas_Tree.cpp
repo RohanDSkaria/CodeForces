5041bd1d58d5400973fd670743659ce901794291
RohanDSkari <rohandanny484@gmail.com>
2024-08-04 04:22:56 +0530

    cnt++

--- D_Rudolph_and_Christmas_Tree.cpp
+++ D_Rudolph_and_Christmas_Tree.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define IOS ios::sync_with_stdio(0);cin.tie(nullptr);cout.tie(nullptr);
 #define endl '\n'
-#define int long long
+#define int double
 #define pb(a) push_back(a)
 #define v vector
 #define vi v<int>
@@ -13,12 +13,23 @@ template<typename T>istream& operator>>(istream& is, v<T>& v){for(auto& x : v)is
 template<typename T>ostream& operator<<(ostream& os, v<T>& v){for(auto& x : v)os << x << ' ';return os;}
 
 void solve(){
-   
+    int n,d,h;cin>>n>>d>>h;
+    vi a(n);cin>>a;
+    int k=(d*h)/2,ans=0;
+    for(int i=0; i<n-1; i++){
+        if(a[i]+h>a[i+1]){
+            int nh=a[i]+h-a[i+1];
+            int ns=(nh*d)/h;
+            ans+=(ns+d)*(a[i+1]-a[i])/2;
+        }
+        else ans+=k;
+    }
+    cout<<setprecision(20)<<ans+k<<endl;
 }
 int32_t main(){
-   IOS int t=1;
-   cin>>t;
-   while(t--) solve();
+    IOS int t=1;
+    cin>>t;
+    while(t--) solve();
 }
 /*
 
